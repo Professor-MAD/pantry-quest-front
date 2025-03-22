c725838a3b1691843ee624890a43ea3cd24e9905
Professor MAD <professormad@proton.me>
2025-03-22 07:36:40 -0400

    add nav footer to recipes

--- screens/RecipeScreen.tsx
+++ screens/RecipeScreen.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import NavFooter from "@/components/NavFooter";
 
-export default function RecipeScreen() {
+export default function RecipeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>This is the Recipes Page</Text>
+      <View style={styles.navFooterRecipe}>
+        <NavFooter navigation={navigation} />
+      </View>
     </View>
   );
 }
@@ -19,5 +23,12 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "bold",
+  },
+  navFooterRecipe: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    height: 80,
+    zIndex: 10,
   },
 });
