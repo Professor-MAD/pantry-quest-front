dbb3a8952104394235056d8189db94bd978e5c6a
Professor MAD <professormad@proton.me>
2025-03-25 20:20:34 -0400

    add pantry provider wrap to app

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ import RecipeScreen from "./screens/RecipeScreen";
 import FridgeScreen from "./screens/FridgeScreen";
 import * as SplashScreen from "expo-splash-screen";
 import { useFonts } from "expo-font";
+import { PantryProvider } from "./components/PantryContext/PantryContext";
 
 const Stack = createStackNavigator();
 SplashScreen.preventAutoHideAsync();
@@ -37,16 +38,18 @@ export default function App() {
 
   return (
     <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Pantry" component={PantryScreen} />
-          <Stack.Screen name="Grocery" component={GroceryListScreen} />
-          <Stack.Screen name="Settings" component={SettingsScreen} />
-          <Stack.Screen name="Recipes" component={RecipeScreen} />
-          <Stack.Screen name="Fridge" component={FridgeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PantryProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Pantry" component={PantryScreen} />
+            <Stack.Screen name="Grocery" component={GroceryListScreen} />
+            <Stack.Screen name="Settings" component={SettingsScreen} />
+            <Stack.Screen name="Recipes" component={RecipeScreen} />
+            <Stack.Screen name="Fridge" component={FridgeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PantryProvider>
     </View>
   );
 }
